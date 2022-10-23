0d2abcb87a33f4d208c99569c44b64e89987963f
redgie <102281283+redgie16@users.noreply.github.com>
2022-10-23 22:47:14 +0800

    Add files via upload

--- midterm/app.py
+++ midterm/app.py
@@ -1,62 +1,147 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, session, g
 from flask.templating import render_template
 from database import get_database
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+@app.teardown_appcontext
+def close_database(error):
+    if hasattr(g, 'crudapplication_db'):
+        g.crudapplication_db.close()
+
+def get_current_user():
+    user = None
+    if 'user' in session:
+        user = session['user']
+        db = get_database()
+        user_cur = db.execute('SELECT * FROM users WHERE name = ?', [user])
+        user = user_cur.fetchone()
+    return user
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    user = get_current_user()
+    return render_template('home.html', user = user)
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=["POST", "GET"])
 def login():
+    user = get_current_user()
     error = None
+    db = get_database()
     if request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
-        db = get_database()
         user_cursor = db.execute('SELECT * FROM users WHERE name = ?', [name])
         user = user_cursor.fetchone()
 
         if user:
             if check_password_hash(user['password'], password):
+                session['user'] = user['name']
                 return redirect(url_for('dashboard'))
             else:
-                error = " user/password did not match"
-    return render_template('login.html', loginerror = error)
+                error = "Username or Password did not match."
+        else:
+            error = 'Username or password did not match.'
+    return render_template('login.html', loginerror = error, user = user)
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
+    user = get_current_user()
+    db = get_database()
     if request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
         hashed_password = generate_password_hash(password)
         
-        db = get_database()
+        dbuser_cur = db.execute('SELECT * FROM users WHERE name = ?', [name])
+        existing_user = dbuser_cur.fetchone()
+        if existing_user:
+            return render_template('register.html', register_error = 'Username already taken')
+
         db.execute('INSERT INTO users (name, password) VALUES (?, ?)', [name, hashed_password])
         db.commit()
         return redirect(url_for('index'))
-    return render_template('register.html')
+    return render_template('register.html', user = user)
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    user = get_current_user()
+    db = get_database()
+    if request.method == "GET":
+        emp_cur = db.execute('SELECT * FROM emp')
+        allemp = emp_cur.fetchall()
 
-@app.route('/addnewemployee')
+    return render_template('dashboard.html', user = user, allemp = allemp)
+
+@app.route('/addnewemployee', methods = ["POST", "GET"])
 def addnewemployee():
-    return render_template('addnewemployee.html')
+    user = get_current_user()
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        address = request.form['address']
 
-@app.route('/singleemployee')
-def singleemployee():
-    return render_template('singleemployee.html')
+        db = get_database()
+        db.execute('INSERT INTO emp (name, email, phone, address) VALUES (?,?,?,?)', [name, email, phone, address])
+        db.commit()
+        return redirect(url_for('dashboard'))
 
-@app.route('/updateemployee')
+    return render_template('addnewemployee.html', user = user)
+
+@app.route('/singleemployee/<int:empid>')
+def singleemployee(empid):
+    user = get_current_user()
+    db = get_database()
+    emp_cur = db.execute('SELECT * FROM emp WHERE empid = ?', [empid])
+    single_emp = emp_cur.fetchone()
+
+    return render_template('singleemployee.html', user = user, single_emp = single_emp)
+
+@app.route('/fetchone/<int:empid>')
+def fetchone(empid):
+    user = get_current_user()
+    db = get_database()
+    emp_cur = db.execute('SELECT * FROM emp WHERE empid = ?', [empid])
+    single_emp = emp_cur.fetchone()
+
+    return render_template('updateemployee.html', user = user, single_emp = single_emp)
+
+@app.route('/updateemployee', methods = ["POST" , "GET"])
 def updateemployee():
-    return render_template('updateemployee.html')
+    user = get_current_user()
+    if request.method == "POST":
+        empid = request.form['empid']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        address = request.form['address']
 
+        db = get_database()
+        db.execute('UPDATE emp SET name = ?, email = ?, phone = ?, address = ? WHERE empid = ?', [name, email, phone, address, empid])
+        db.commit()
+        return redirect(url_for('dashboard'))
 
+    return render_template('updateemployee.html', user = user)
+
+@app.route('/deleteemployee/<int:empid>', methods = ["POST", "GET"])
+def deleteemployee(empid):
+    user = get_current_user()
+    if request.method == "GET":
+        db = get_database()
+        db.execute('DELETE FROM emp WHERE empid = ?', [empid])
+        db.commit()
+        return redirect(url_for('dashboard'))
+        
+    return render_template('dashboard.html', user = user)
+
+@app.route('/logout')
 def logout():
+    session.pop('user', None)
     render_template('home.html')
 
 if __name__ == '__main__':
